84db690dd9eb8a13958f0472d9d289d36e010120
AlbatovK <albatovkonstantin@yandex.ru>
2022-07-13 23:18:49 +0600

    :art: Heroku stable

--- src/main/kotlin/com/albatros/diraserver/DiraserverApplication.kt
+++ src/main/kotlin/com/albatros/diraserver/DiraserverApplication.kt
@@ -2,10 +2,18 @@ package com.albatros.diraserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DiraserverApplication
 
 fun main(args: Array<String>) {
 	runApplication<DiraserverApplication>(*args)
+}
+
+@RestController
+class Controller {
+	@GetMapping("/api/version")
+	fun getApiVersion() = "1.0.0"
 }
